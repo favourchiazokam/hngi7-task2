71491b3f85da97a2e63b89ace4d7bc864b03718a
favourchiazokam <52789965+favourchiazokam@users.noreply.github.com>
2020-06-07 08:40:14 +0100

    Add files via upload
    js task2 done

---
+++ scripts/task2.js
@@ -0,0 +1,14 @@
+var myEmail = "favchiazokam@gmail.com";
+var myName = "Favour Igwe";
+var myId = "HNG-01067";
+var myLanguage = "javascript";
+console.log(
+  "Hello World, this is " +
+    myName +
+    " with HNGi7 ID " +
+    myId +
+    " using " +
+    myLanguage +
+    " for stage 2 task email " +
+    myEmail
+);
